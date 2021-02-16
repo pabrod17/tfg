2f0edc7d8c1673498bf629959cd1bd8632d89e8d
Pablo Rodríguez Miñambres <pablo.rminambres@udc.es>
2021-02-16 19:37:54 +0100

    Remove Query sql of findSeasonTeamsBySeasonId and findSeasonTeamsByTeamId

--- backend/src/main/java/es/udc/paproject/backend/model/entities/SeasonTeamDao.java
+++ backend/src/main/java/es/udc/paproject/backend/model/entities/SeasonTeamDao.java
@@ -2,16 +2,13 @@ package es.udc.paproject.backend.model.entities;
 
 import java.util.List;
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
 public interface SeasonTeamDao extends PagingAndSortingRepository<SeasonTeam, Long>{
 
     //Aqui accedo a los seasonTeam de una temporada y de ahi saco los teams de una temporada
-    @Query("SELECT s FROM SeasonTeam s WHERE seasonId = ?1")
     List<SeasonTeam> findSeasonTeamsBySeasonId(Long seasonId);
 
-    @Query("SELECT s FROM SeasonTeam s WHERE teamId = ?1")
     List<SeasonTeam> findSeasonTeamsByTeamId(Long teamId);
 
     boolean existsByTeamId(Long teamId);
