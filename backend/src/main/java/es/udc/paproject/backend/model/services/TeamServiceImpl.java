595aa4a42aca40213bce65ebbf5ff0ab60f55dc4
Pablo Rodríguez Miñambres <pablo.rminambres@udc.es>
2021-03-25 00:21:54 +0100

    Remove warnings

--- backend/src/main/java/es/udc/paproject/backend/model/services/TeamServiceImpl.java
+++ backend/src/main/java/es/udc/paproject/backend/model/services/TeamServiceImpl.java
@@ -1,10 +1,8 @@
 package es.udc.paproject.backend.model.services;
 
 import java.util.ArrayList;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Optional;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
